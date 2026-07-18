54c47e4f4f69d928c57fa59a37f2c8116da4ea4c
Alice Ford <aliceford28@gmail.com>
2026-07-18 15:42:30 -0400

    new changes

---
+++ new.js
@@ -0,0 +1,29 @@
+// let i = 2
+// for (i = 2; i <= 8; i++) { console.log(i); }
+// for (i = 2; i <= 35; i+=4) { console.log(i); }
+// let a=1;
+// for(let i=3; i<=8; i++) { a*=i; }
+// console.log(a);
+// const person = {
+//     Firstname: "ALice",
+//     Lastname: "ford",
+//     Age: 46
+// };
+// console.log(person.Firstname + " " + person.Lastname);
+// let salary = 1200; bonus = 0;
+// if (salary == 1000) { bonus = 20 % salary; }
+// else if (salary > 1000 && salary < 2000) { bonus = 10 % salary }
+// else if (salary >= 2000 && salary <= 5000) { bonus = 5 % salary }
+// else if (salary > 5000) { bonus = 0 }
+// console.log("ბონუსია:" + bonus);
+const Fruits = ["Apple", "bannana", "Orange"]
+// for (let fruit of Fruits) 
+//     {console.log(fruit) };
+// let sum = 0;
+// for (let i=1; i<=34; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+Fruits.unshift("Grape");
+Fruits.unshift("Pineapple");
+console.log(Fruits);
