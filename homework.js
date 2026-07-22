1fd2d2e24fdc2a35ad8cc6294fa8d7e669c05633
Alice Ford <aliceford28@gmail.com>
2026-07-22 08:49:27 -0400

    1

--- homework.js
+++ homework.js
@@ -1,10 +1,42 @@
-// დაწერეთ ფუნქცია რომელსაც გადასცემ ორ რიცხვს (m,n) და დააბრუნებს რომელიც მეტია იმ რიცხვს , თუ რიცხვები ტოლია დააბრუნებს 0 -ს
-function get_greater(m, n) {
-    if (m > n) {
-        return m;
-    } else if (n > m) {
-        return n;
-    } else {
-        return 0;
-    }
-}
+// function getmax(m, n) {
+//     if (m > n) {
+//         return m;
+//     } else if (m < n) {
+//         return n;
+//     } else {
+//         return 0;
+//     }
+// }
+// // console.log(getmax(5, 10));
+// // console.log(getmax(8, 4));
+// console.log(getmax(3, 3 ));
+// function sum(x, y) {return x+y;}
+// console.log(sum(20, 30));
+// function printname(){console.log('Alice Ford');}
+// printname();
+// function getfullname(firstname, lastname) {return firstname + ' ' + lastname;} 
+// const fullname = getfullname('Alice', 'Ford');
+// console.log(fullname);
+// function getfactorial(n) {
+//     let factorial = 1;
+//     for(let i=1; i<=n; i++){
+//         factorial *= i;
+//     }
+// //     return factorial;
+// // }
+// // console.log(getfactorial(8));
+// const student = {
+//     firstname: 'Alice', lastname: 'Ford', Age: 46,
+//     scores: [4, 7, 5, 3, 2]
+// };
+// // console.log(student.firstname + ' ' + student.lastname + ' ' + student.Age);
+// // console.log(student.firstname);
+// // console.log(student.lastname);
+// function getsum(scores) {
+//     let sum = 0;
+//     for (let i = 0; i < scores.length; i++) {
+//         sum += scores[i];
+//     }
+//     return sum;
+// }
+// console.log(getsum(student.scores));
