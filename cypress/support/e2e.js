c607a24215eb88a157e5d585774c98a9829421f4
Alice Ford <aliceford28@gmail.com>
2026-08-13 15:47:40 -0400

    1

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -48,3 +48,42 @@ cy.contains('a', 'Logged in as aliceford').should('be.visible');
 import './commands'
 import './automation_exercise' {
 }'
+
+
+2. Navigate to url 'http://automationexercise.com'
+cy.visit('http://automationexercise.com');
+3. Verify that home page is visible successfully
+cy.get('header').should('be.visible');
+4. Click on 'Signup / Login' button
+cy.contains ('a', 'Signup / Login').click();
+5. Verify 'Login to your account' is visible
+cy.contains('h2', 'Login to your account').should('be.visible');
+6. Enter correct email address and password
+cy.get('input[name="email"]').type('aliceford@example.com');
+cy.get('input[name="password"]').type('aliceford123!');
+7. Click 'login' button
+cy.get('[data-qa="login-button"]').click(); 
+8. Verify that 'Logged in as username' is visible
+cy.contains('a', 'Logged in as aliceford').should('be.visible');
+9. Click 'Delete Account' button
+cy.contains('a', 'Delete Account').click();
+10. Verify that 'ACCOUNT DELETED!' is visible
+cy.contains('h2', 'ACCOUNT DELETED!').should('be.visible');
+
+1. Launch browser
+cy.visit('about:blank');
+2. Navigate to url 'http://automationexercise.com'
+cy.visit('http://automationexercise.com');
+3. Verify that home page is visible successfully
+cy.get('header').should('be.visible');
+4. Click on 'Signup / Login' button
+cy.contains ('a', 'Signup / Login').click();
+5. Verify 'Login to your account' is visible
+cy.contains('h2', 'Login to your account').should('be.visible');
+6. Enter incorrect email address and password
+cy.get('input[name="email"]').type('koko1@example.com');
+cy.get('input[name="password"]').type('koko123');
+7. Click 'login' button
+cy.get('[data-qa="login-button"]').click();
+8. Verify error 'Your email or password is incorrect!' is visible
+cy.contains('p', 'Your email or password is incorrect!').should('be.visible');
