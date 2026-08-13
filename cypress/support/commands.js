5c9c241e9302212f15d9918b2942cb45fdd9dd93
Alice Ford <aliceford28@gmail.com>
2026-08-13 15:27:27 -0400

    2

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 //login custom command
 Cypress.Commands.add("login", (email, name, password) => {
-    cy.get("a[[href=" / login"]').click();  
+    cy.get('a[href="/login"]').click();
     cy.get('[data-cy="email"]').type(email);
     cy.get('[data-cy="name"]').type(name);
     cy.get('[data-cy="password"]').type(password);
@@ -8,9 +8,9 @@ Cypress.Commands.add("login", (email, name, password) => {
 });
 //registration step 1 (name,email.password)
 Cypress.Commands.add("register", (name, email, password) => {
-    cy.get("a[[href=" / login"]').click();
+    cy.get('a[href="/login"]').click();
     cy.get('[data-cy="name"]').type(name);
     cy.get('[data-cy="email"]').type(email);
     cy.get('[data-cy="password"]').type(password);
     cy.get('[data-cy="signup-button"]').click();
-}
+});
