696668d7dfb7ff39c1fae83d8f0b33abdfd82e50
Alice Ford <aliceford28@gmail.com>
2026-08-12 21:20:44 -0400

    1

---
+++ cypress/e2e/third test file.cy.js
@@ -0,0 +1,16 @@
+Test Case 1: Register User
+Test Case 4: Logout User
+Test Case 5: Register User with existing email
+Test Case 6: Contact Us Form
+Test Case 7: Verify Test Cases Page
+Test Case 8: Verify All Products and product detail page
+Test Case 9: Search Product
+Test Case 10: Verify Subscription in home page
+Test Case 11: Verify Subscription in Cart page
+Test Case 12: Add Products in Cart
+Test Case 13: Verify Product quantity in Cart
+Test Case 14: Place Order: Register while Checkout
+Test Case 15: Place Order: Register before Checkout
+Test Case 16: Place Order: Login before Checkout
+Test Case 2: Login User with correct email and password
+Test Case 3: Login User with incorrect email and password
